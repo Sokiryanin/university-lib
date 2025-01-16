aba4d7bd0e24ab026aeaa76ba825a1fdda252a58
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2025-01-16 21:33:32 +0100

    add ui auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import BookList from '@/components/BookList';
 import BookOverview from '@/components/BookOverview';
-// import { Button } from '@/components/ui/button';
+ import { Button } from '@/components/ui/button';
 import { sampleBooks } from '@/constants';
 
 const Home = () => (
